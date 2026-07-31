c22a34c965c2788e92b4fd3dea45de986668fc53
JoseVides17 <jose.videsbaron@gmail.com>
2026-07-31 15:13:27 -0500

    WalletEntity: Implement Persistable interface

--- src/main/java/com/bykeeasy/infrastructure/adapter/out/persistence/entity/WalletEntity.java
+++ src/main/java/com/bykeeasy/infrastructure/adapter/out/persistence/entity/WalletEntity.java
@@ -2,12 +2,14 @@ package com.bykeeasy.infrastructure.adapter.out.persistence.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.domain.Persistable;
+
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "wallets")
 @Data
-public class WalletEntity {
+public class WalletEntity implements Persistable<String> {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -17,4 +19,25 @@ public class WalletEntity {
     private UserEntity user;
 
     private BigDecimal balance;
+
+    @Transient // No se persiste en la BD
+    private boolean isNew = true;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
+
+    // Callbacks de JPA para marcar la entidad como persistida
+    @PostLoad
+    @PostPersist
+    void markNotNew() {
+        this.isNew = false;
+    }
+
 }
